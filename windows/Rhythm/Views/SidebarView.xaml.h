920644d7d68dd99c15257b79d91b83ed43606691
Terry <czx123czx000@outlook.com>
2026-09-15 01:45:15 +1000

    chore: #383 删除无人可达的侧栏控件与其构建登记（版本 0.5.157）
    问题：SidebarView 没有任何调用方，主窗口用 NavigationView 做侧栏，它却仍被编译并被包含头文件。
    
    解决：删除该控件的三个文件、构建登记与主窗口的头文件包含。

--- windows/Rhythm/Views/SidebarView.xaml.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace winrt::Rhythm::Views::implementation {
-struct SidebarView : winrt::Microsoft::UI::Xaml::Controls::UserControlT<SidebarView> {
-    SidebarView();
-    void OnSelectionChanged(winrt::Windows::Foundation::IInspectable const&,
-                            winrt::Microsoft::UI::Xaml::Controls::SelectionChangedEventArgs const&);
-};
-} // namespace winrt::Rhythm::Views::implementation
